1c560433427bf9ed9f9667e410e24cf59263df0b
GuBee33 <74045047+GuBee33@users.noreply.github.com>
2025-02-06 01:13:23 +0100

    new services added

--- services.py
+++ services.py
@@ -9,34 +9,41 @@ import re
 os.environ['OPENAI_API_KEY'] = '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'
 
 class DataLoader:
-    def __init__(self, file_path,limit):
-        self.file_path = file_path
-        self.limit=limit
+    def __init__(self, file_path):
+        self.df=pd.read_excel(file_path)
 
-    def load_data(self):
-        with open(self.file_path, 'r') as file:
-            lines = file.readlines()
-        question = lines[0].strip()
-        answers = [line.strip() for line in lines[1:self.limit+1]]
-        return question, answers
+    def get_columnIds(self):
+        for index, element in enumerate(self.df.columns):
+            print(index, element)
+
+    def get_answers(self,columnId,limit=None):
+        question=self.df.columns[columnId]
+        return self.df[question].str.replace('\n', '', regex=False).tolist()[:limit]
 
 class Categorizer:
-    def __init__(self, categories, api_client,model="gpt-4o"):
-        self.categories_dict = {category: [] for category in categories}
-        self.api_client = api_client
+    def __init__(self,model="gpt-4o"):
+        self.categories_dict = {}
+        self.answers_categories={}
+        self.api_client = OpenAI()
         self.model = model
 
-    def categorize_answers(self, answers):
+    def categorize_answers(self, question,answers,categories,max_categories_per_answer=3):
+        self.categories_dict[question]={category: [] for category in categories}
+        if f"{question}_2" not in self.answers_categories:
+            self.answers_categories[f"{question}_2"]=[]
+        for i in range(max_categories_per_answer):
+            if f"{question}_category_{i}" not in self.answers_categories:
+                self.answers_categories[f"{question}_category_{i}"]=[]
         formatted_answers = "\n".join([f"{i+1}. {answer}" for i, answer in enumerate(answers)])
         system_prompt = (
             "You will receive multiple employee survey answers and a list of existing categories. "
-            "For each answer, assign up to 3 relevant category labels from the existing categories. "
+            f"For each answer, assign up to {max_categories_per_answer} relevant category labels from the existing categories or if really not fitting then you can add a new category. Do not use similar categories"
             "Provide the results as a JSON array where each element is an array of categories corresponding to each answer. "
-            "the response must contain only the JSON array no other string or leading/ending character"
-            "Use only the Hungarian language for the category labels."
+            "the response must contain only the JSON array no other string or leading/ending character. No markdown format or any extra edded text"
+            "Use the same language for the category labels as the answer has."
         )
         user_prompt = (
-            f"Existing categories: [{', '.join(self.categories_dict.keys())}].\n\n"
+            f"Existing categories: [{', '.join(self.categories_dict[question].keys())}].\n\n"
             f"Survey Answers:\n{formatted_answers}\n\n"
             "Please provide the category assignments as specified."
         )
@@ -55,16 +62,22 @@ class Categorizer:
         categories_list = json.loads(response_text)
         for i in range(len(answers)):
             categories = categories_list[i]
-            for category in categories:
-                if category in self.categories_dict:
-                    self.categories_dict[category].append(answers[i])
+            self.answers_categories[f"{question}_2"].append(answers[i])
+            for j in range(max_categories_per_answer):
+                category=None
+                if len(categories)>j:
+                    category=categories[j]
+                self.answers_categories[f"{question}_category_{j}"].append(category)
+                if category in self.categories_dict[question]:
+                    self.categories_dict[question][category].append(answers[i])
                 else:
-                    self.categories_dict[category] = [answers[i]]
-        return self.categories_dict
+                    self.categories_dict[question][category] = [answers[i]]
 
-    def find_keys_by_value(self, target_value):
+        return self.categories_dict, self.answers_categories
+
+    def find_keys_by_value(self, question,target_value):
         matching_keys = []
-        for key, values_list in self.categories_dict.items():
+        for key, values_list in self.categories_dict[question].items():
             if target_value in values_list:
                 matching_keys.append(key)
         return matching_keys
@@ -76,14 +89,10 @@ class Chunker:
         for i in range(0, len(data), self.chunk_size):
             yield data[i:i + self.chunk_size]
 
-class Exporter:
-    def __init__(self, categorizor, answers):
-        self.categorizer = categorizor
-        self.answers = answers
-    
-    def sanitize_filename(self,name):
+class Exporter: 
+    def sanitize_string(self,name):
         """
-        Sanitize a string to be a valid filename by removing or replacing invalid characters.
+        Sanitize a string to be a valid filename ot sheetname by removing or replacing invalid characters.
         """
         # Define a pattern for invalid filename characters
         invalid_chars = r'[\s<>:"/\\|?*]'
@@ -96,25 +105,17 @@ class Exporter:
         """
         Convert a random string to a valid .xlsx filename.
         """
-        sanitized = self.sanitize_filename(name)
+        sanitized = self.sanitize_string(name)
         # Ensure the filename ends with .xlsx
         if not sanitized.lower().endswith('.xlsx'):
             sanitized += '.xlsx'
         return sanitized
 
-    def dump_to_excel(self, df_sorted, sheet_name='Answers and Categories' ,excel_file='result_data.xlsx'):
+    def dump_to_excel(self, dfs ,excel_file='result_data.xlsx'):
         excel_file=self.make_valid_xlsx_filename(excel_file)
-        sheet_name=self.sanitize_filename(sheet_name)
-        data = []
-        for answer in self.answers:
-            categories = self.categorizer.find_keys_by_value(answer)
-            data.append([answer] + categories)
-        max_categories = max(len(item) - 1 for item in data)
-        column_names = ['Answer'] + [f'Category{i+1}' for i in range(max_categories)]
-        df = pd.DataFrame(data, columns=column_names)
         with pd.ExcelWriter(excel_file) as writer:
-            df.to_excel(writer, sheet_name=sheet_name[:15], index=False)
-            df_sorted.to_excel(writer, sheet_name='Summary', index=False)
+            for sheetname,df in dfs.items():
+                df.to_excel(writer, sheet_name=self.sanitize_string(sheetname), index=False)
         print(f"Data has been written to '{excel_file}'.")
 
     def plot_result(self, df_sorted):
@@ -131,13 +132,15 @@ class Exporter:
         plt.show()
 
 class SurveyAnalyzer:
-    def __init__(self, file_path, categories,model="gpt-4o",result_excel='result_data.xlsx',answer_limit=None,chunk_size=10):
-        self.loader = DataLoader(file_path,answer_limit)
-        self.client = OpenAI()
-        self.categorizer = Categorizer(categories, self.client,model)
+    def __init__(self, file_path,model="gpt-4o",result_excel='result_data.xlsx',answer_limit=None,chunk_size=10):
+        self.loader = DataLoader(file_path)
+        self.answer_limit=answer_limit
+        self.categorizer = Categorizer(model)
         self.chunker = Chunker(chunk_size)
         self.excel_file = result_excel
         self.chunk_size=chunk_size
+        self.loader.get_columnIds()
+        self.dfs_2_excel={}
 
     def find_keys_by_value(self, target_value):
         return self.categorizer.find_keys_by_value(target_value)
@@ -147,17 +150,33 @@ class SurveyAnalyzer:
         df = pd.DataFrame(lengths.items(), columns=['Category', 'Length'])
         return df.sort_values(by='Length', ascending=False)
 
-    def categorize_all_answers(self, answers):
-        for answer_chunk in tqdm(self.chunker.chunk_list(answers), desc="Categorizing answers", unit=" chunk", total=round(len(answers)/self.chunk_size)):
-            self.categorizer.categorize_answers(answer_chunk)
+    def categorize_all_answers(self, question,answers,categories,max_categories_per_answer):
+        for answer_chunk in tqdm(self.chunker.chunk_list(answers), desc=f"Categorizing answers for {question[:15]}...", unit=" chunk", total=round(len(answers)/self.chunk_size)):
+            self.categorizer.categorize_answers(question,answer_chunk,categories,max_categories_per_answer)
 
-    def export_results(self,question):
-        exporter = Exporter(self.categorizer, self.answers)
-        df_sorted = self.sorted_summary_df()
-        exporter.dump_to_excel(df_sorted,question, self.excel_file)
-        exporter.plot_result(df_sorted)
+    def export_results(self):
+        exporter = Exporter()
+        exporter.dump_to_excel(self.dfs_2_excel, self.excel_file)
+        # exporter.plot_result(df_sorted)
 
-    def run(self):
-        question, self.answers = self.loader.load_data()
-        self.categorize_all_answers(self.answers)
-        self.export_results(question)
+    def aggregate_categories(self,column_id_of_question,column_id_of_aggregate_by_id,number_of_max_categories):
+        question=self.loader.df.columns[column_id_of_question]
+        aggregate_by=self.loader.df.columns[column_id_of_aggregate_by_id]
+        categories=[f"{question}_category_{i}" for i in range(number_of_max_categories)]
+        stat_df_groupby= self.loader.df.melt(id_vars=[question, aggregate_by], value_vars=categories,
+                        var_name='CategoryType', value_name='Category').drop('CategoryType', axis=1).groupby([aggregate_by,'Category']).count().reset_index().sort_values([aggregate_by,question],ascending=False)
+        stat_df=stat_df_groupby.groupby("Category")[question].sum().reset_index().sort_values(question,ascending=False)
+        return {f"{question[:15]}_stat_groupby":stat_df_groupby,f"{question[:15]}_stat":stat_df}
+
+    def run(self,columnIds,categories_per_columnId,max_categories_per_answer,aggregation_column_id):
+        for columnId in columnIds:
+            question=self.loader.df.columns[columnId]
+            self.answers = self.loader.get_answers(columnId,self.answer_limit)
+            self.categorize_all_answers(question,self.answers,categories_per_columnId[columnId],max_categories_per_answer)
+        self.loader.df=pd.concat([self.loader.df,pd.DataFrame(self.categorizer.answers_categories)],axis=1)
+        self.dfs_2_excel = self.dfs_2_excel | {"rawdata":self.loader.df}
+        
+        for columnId in columnIds:
+            self.dfs_2_excel = self.dfs_2_excel | self.aggregate_categories(columnId,aggregation_column_id,max_categories_per_answer)
+        self.export_results()
+        # self.export_results(question)
